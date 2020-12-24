6d2d12ac1f907db7170482d39e49fd8e9d16ccf6
aleks.marchenko <aleks.marchenko802@gmail.com>
2020-12-24 23:48:00 +0200

    add adapter

---
+++ src/structuralPatterns/adapter/Database.java
@@ -0,0 +1,12 @@
+package structuralPatterns.adapter;
+
+public interface Database {
+
+    void insert();
+
+    void update();
+
+    void select();
+
+    void remove();
+}
---
+++ src/structuralPatterns/adapter/JavaApplication.java
@@ -0,0 +1,20 @@
+package structuralPatterns.adapter;
+
+public class JavaApplication {
+
+    public void saveObject(){
+        System.out.println("Save Java object...");
+    }
+
+    public void updateObject(){
+        System.out.println("Update Java object...");
+    }
+
+    public void selectObject(){
+        System.out.println("Load Java object...");
+    }
+
+    public void removeObject(){
+        System.out.println("Delete Java object...");
+    }
+}
